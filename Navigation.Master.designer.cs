020f2dbc0fbce2fb6e11bca9cecd272df7c70003
hythenn <kennethmagaling2003@gmail.com>
2026-03-20 21:19:00 +0800

    MAGALING - Fixed Mapua Name

--- Navigation.Master.designer.cs
+++ Navigation.Master.designer.cs
@@ -24,6 +24,15 @@ namespace Music_Studio_Booking
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// phPublicLinks control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder phPublicLinks;
+
+        /// <summary>
         /// phGuest control.
         /// </summary>
         /// <remarks>
@@ -46,25 +55,9 @@ namespace Music_Studio_Booking
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder phAdmin;
-
-        /// <summary>
-        /// phPublicLinks control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.PlaceHolder phPublicLinks;
-
-        /// <summary>
-        /// btnLogout control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton btnLogout;
+        protected global::System.Web.UI.WebControls.PlaceHolder phAdmin;
 
         /// <summary>
         /// ContentPlaceHolder1 control.
